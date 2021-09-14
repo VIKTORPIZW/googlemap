d225201051d58365046e7eded04ffc709eec46cd
VIKTORPIZW <VIKTORPIZW@mail.ru>
2021-09-14 23:14:37 +0300

    Home Task 2 (Recycler done 1.2)

--- clever_2/src/main/java/com/example/clever_2/MainActivity.kt
+++ clever_2/src/main/java/com/example/clever_2/MainActivity.kt
@@ -2,38 +2,22 @@ package com.example.clever_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+            override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+                setContentView(R.layout.activity_main)
 
-        val rclNames = findViewById<RecyclerView>(R.id.rclNames)
-        rclNames.setHasFixedSize(true)
-        val nameList = getListOfNames()
-        val descList = getListOfDescriptions()
-        val namesAdapter = NameAdapter(nameList,descList)
-        rclNames.adapter = namesAdapter
-        rclNames.layoutManager = LinearLayoutManager(this)
 
-    }
+            val fragment: Fragment = MainFragment.newInstance()
+                val transaction = supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.fragment_container,fragment, "main_fragment")
+                transaction.commit()
 
-    private fun getListOfNames(): MutableList<String> {
-        val nameList = mutableListOf<String>()
-        for (i in 1..1000)
-            nameList.add("Title $i")
-        return nameList
 
     }
-    private fun getListOfDescriptions(): MutableList<String> {
-        val descriptionList = mutableListOf<String>()
-        for (i in 1..1000)
-            descriptionList.add("Description $i")
-        return descriptionList
-
-    }
-
 }
---
+++ clever_2/src/main/java/com/example/clever_2/MainFragment.kt
@@ -0,0 +1,59 @@
+package com.example.clever_2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.util.*
+
+class MainFragment : Fragment(), RecyclerViewAdapter.ClickListener{
+    private lateinit var adapter: RecyclerViewAdapter
+    val listData: LinkedList<DataModel> = LinkedList()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
+        buildDisplayData()
+        initRecyclerView(view)
+        return view
+    }
+    private fun initRecyclerView (view: View){
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+        adapter = RecyclerViewAdapter(listData,this)
+        recyclerView.adapter = adapter
+
+    }
+    private fun buildDisplayData (){
+        for (i in 1 .. 1000)
+            listData.add(DataModel("Title $i"))
+
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance() =
+            MainFragment().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+
+    override fun onItemClick(dataModel: DataModel) {
+        TODO("Not yet implemented")
+    }
+}
