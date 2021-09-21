c32b1204b37f7c880b591e1b3f0ef799b1537b86
VIKTORPIZW <VIKTORPIZW@mail.ru>
2021-09-22 00:40:19 +0300

    Home Task 3(test)

--- clever_3/src/main/java/com/example/clever_3/MainActivity.kt
+++ clever_3/src/main/java/com/example/clever_3/MainActivity.kt
@@ -1,11 +1,65 @@
 package com.example.clever_3
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.view.View
+import android.widget.Button
+import android.widget.ListView
+import android.widget.SimpleCursorAdapter
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+
 
 class MainActivity : AppCompatActivity() {
+    var cols = listOf<String>(
+        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+        ContactsContract.CommonDataKinds.Phone.NUMBER,
+        ContactsContract.CommonDataKinds.Phone._ID
+    ).toTypedArray()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonSelect :Button
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.READ_CONTACTS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                Array(1) { android.Manifest.permission.READ_CONTACTS },
+                123
+            )
+        } else readContacts()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 123 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            readContacts()
+    }
+
+    private fun readContacts() {
+        var from = listOf<String>(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+            ContactsContract.CommonDataKinds.Phone.NUMBER).toTypedArray()
+        var to = intArrayOf(android.R.id.text1,android.R.id.text2)
+        val rs = contentResolver.query(
+            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+            cols,
+            null,
+            null,
+            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME
+        )
+        var adapter = SimpleCursorAdapter(this,android.R.layout.simple_list_item_1,rs,from, to,0)
+        var listView :ListView = findViewById(R.id.listview1)
+        listView.adapter = adapter
+
     }
 }
