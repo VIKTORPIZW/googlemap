dafc6149f8f4883a3341ab9d9acf880a6a36d550
VIKTORPIZW <VIKTORPIZW@mail.ru>
2021-09-23 02:55:48 +0300

    Home Task 3(test)

--- clever_3/src/main/java/com/example/clever_3/MainActivity.kt
+++ clever_3/src/main/java/com/example/clever_3/MainActivity.kt
@@ -1,24 +1,77 @@
 package com.example.clever_3
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
+import android.widget.SimpleCursorAdapter
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.example.clever_3.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+
+    var cols = listOf<String>(
+        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+        ContactsContract.CommonDataKinds.Phone.NUMBER,
+        ContactsContract.CommonDataKinds.Phone._ID
+    ).toTypedArray()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val listView1:ListView = findViewById(R.id.listView)
+        val buttonSelect: Button = findViewById(R.id.btnSelect)
+        buttonSelect.setOnClickListener {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_CONTACTS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    Array(1) { Manifest.permission.READ_CONTACTS },
+                    111
+                )
+            } else {
+                readContacts()
+            }
+            fun onRequestPermissionsResult(
+                requestCode: Int,
+                permissions: Array<out String>,
+                grantResults: IntArray
+            ) {
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+                if (requestCode == 111 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    readContacts()
+            }
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.btnSelect.setOnClickListener {
 
-            supportFragmentManager.beginTransaction(). replace(
-                R.id.firstFragmentContainer, ListFragment()
-            ).commit()
         }
+
+    }
+
+    private fun readContacts() {
+        var from = listOf<String>(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+            ContactsContract.CommonDataKinds.Phone.NUMBER).toTypedArray()
+        var to = intArrayOf(android.R.id.text1, android.R.id.text2)
+        var rc = contentResolver.query(
+            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+            cols,
+            null,
+            null,
+            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME
+        )
+        var adapter = SimpleCursorAdapter (this,android.R.layout.simple_list_item_1,rc,from,to,0)
+
     }
 }
